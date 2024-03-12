598553f55eb9f1b550f900736c3fb62e4c88f2c1
Harshvardhan Acharya <harshvacharya@gmail.com>
2024-03-13 01:38:21 +0530

    Added genre.py

---
+++ src/routers/genre.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, HTTPException
+from uuid import uuid4
+
+from src.db import Movies
+from src import schemas
+from src.config import config
+from bson.objectid import ObjectId
+
+router=APIRouter()
+
+
+@router.get('/genre/{genre_name}')
+async def get_movie_by_genre(genre_name:str):
+    try:
+        
+        movies = Movies.find({"genres": {'$in':[genre_name]}})
+        movies=await movies.to_list(length=None)
+        ret=[]
+        
+        if movies:
+            for movie in movies:
+                if '_id' in movie:
+                    movie['_id']=str(movie['_id'])
+                ret.append(movie)
+        if ret:
+            return ret
+        raise HTTPException(status_code=404, detail="No movie found for this genre")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
