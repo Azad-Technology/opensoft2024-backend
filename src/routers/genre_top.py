81b11bb2fd938360e0cb3a5aeda6c1652691b9a2
Shivam Kumar <shivamkmishra09@kgpian.iitkgp.ac.in>
2024-03-15 01:09:58 +0530

    region name search made case insensitive , count is now optional (its default value is 10)

--- src/routers/genre_top.py
+++ src/routers/genre_top.py
@@ -5,17 +5,17 @@ from src.db import Movies
 from src import schemas
 from src.config import config
 from bson.objectid import ObjectId
-
+from typing import Optional
 router=APIRouter()
 
-@router.get('/genre_top/{genre_name}/{count}')
-async def get_movies(genre_name:str, count:int):
+@router.get('/genre_top/{genre_name}')     #name has to be changed
+async def get_movies(genre_name:str,  count: Optional[int] = 10):
     
     try:
         if count<1:
            raise HTTPException(status_code=404, detail="Please enter a valid count of movies to be fetched ")
         projection={"_id":1, "title":1, "poster":1, "released": 1, "runtime":1, 'imdb':1, 'tomatoes':1}
-        movies_cur = Movies.find({"imdb.rating":{'$ne':''},"genres": {'$in':[genre_name]}},projection).sort([("imdb.rating", -1)]).limit(count)
+        movies_cur = Movies.find({"imdb.rating":{'$ne':''},"genres": {'$regex': f'^{genre_name}$', '$options': 'i'}},projection).sort([("imdb.rating", -1)]).limit(count)
         movies = await movies_cur.to_list(length=None)
         if movies:
             for movie in movies:
