0820341e4fba75e6d456a42b15b9d38e723844b5
Shivam Kumar <shivamkmishra09@kgpian.iitkgp.ac.in>
2024-03-15 01:09:58 +0530

    region name search made case insensitive , count is now optional (its default value is 10)

--- src/routers/imdb.py
+++ src/routers/imdb.py
@@ -5,13 +5,16 @@ from src.db import Movies
 from src import schemas
 from src.config import config
 from bson.objectid import ObjectId
-
+from typing import Optional
 router=APIRouter()
 
-@router.get('/imdb/{count}')
-async def get_movies(count:int):
+@router.get('/imdb/')
+async def get_movies( count: Optional[int] = 10):
     try:
-        movies_cur = Movies.find({"imdb.rating":{'$ne':''}}).sort([("imdb.rating", -1)]).limit(count)
+        if count<1:
+           raise HTTPException(status_code=404, detail="Please enter a valid count of movies to be fetched ")
+        projection={"_id":1, "title":1, "poster":1, "released": 1, "runtime":1, 'imdb':1, 'tomatoes':1}
+        movies_cur = Movies.find({"imdb.rating":{'$ne':''}},projection).sort([("imdb.rating", -1)]).limit(count)
         movies = await movies_cur.to_list(length=None)
         for movie in movies:
              movie['_id']= str(movie['_id'])
