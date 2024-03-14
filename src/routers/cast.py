84f1330749b0049538cbcfd846eb963789f00610
Harshvardhan Acharya <harshvacharya@gmail.com>
2024-03-15 01:16:36 +0530

    logging

--- src/routers/cast.py
+++ src/routers/cast.py
@@ -10,9 +10,11 @@ router = APIRouter()
 
 @router.get('/cast/{cast_name}')
 async def get_cast(cast_name: str):
+    print(cast_name)
     projection={"_id":1, "title":1, "poster":1, "released": 1, "runtime":1, 'imdb':1, 'tomatoes':1}
     movies = await Movies.find({"cast": {"$elemMatch": {"$eq": str(cast_name)}}}, projection).to_list(length=None)
     # movies = await movie_cursor  # Convert cursor to a list of documents
+    print(movies)
     filtered_movies = []
     for movie in movies:
         if '_id' in movie:
