c6358774c416b7ec424d8365034d5b73b507af0b
Harshvardhan Acharya <harshvacharya@gmail.com>
2024-03-17 01:09:35 +0530

    Fixed auth

--- src/db.py
+++ src/db.py
@@ -2,7 +2,7 @@ from pymongo import mongo_client
 from src.config import config
 from motor.motor_asyncio import AsyncIOMotorClient
 
-client = AsyncIOMotorClient(config['DATABASE_URL'])
+client = AsyncIOMotorClient('mongodb+srv://somya:123@cluster0.yhd6kk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client[config['MONGO_INITDB_DATABASE']]
 print('Connected to MongoDB')
 
@@ -14,3 +14,4 @@ Embedded_movies = db.embedded_movies
 Movies = db.movies
 Sessions = db.sessions
 Theaters = db.theaters
+Movies2=db['movies2']
