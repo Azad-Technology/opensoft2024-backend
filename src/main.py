292e650aa8f91b5d8287837bf3dd27c78bd65cf7
harshsharma2024 <harshsharma2024@gmail.com>
2024-03-20 00:09:36 +0530

    main

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.config import config
 from src.db import db
-from src.routers import movie,cast,genre,search,auth,embeddings,countries,user
+from src.routers import movie,cast,genre,search,auth,countries,user
 import redis
 from fastapi import APIRouter, HTTPException
 from src.cache_system import set_default_ttl
@@ -27,7 +27,7 @@ app.include_router(search.router, tags=["Search"])
 app.include_router(countries.router, tags=["Country Top"])
 app.include_router(auth.router, tags=["Auth"])
 app.include_router(user.router, tags=["Update Info"])
-app.include_router(embeddings.router, tags=["Embeddings"])
+# app.include_router(embeddings.router, tags=["Embeddings"])
 
 @app.get("/")
 async def root():
