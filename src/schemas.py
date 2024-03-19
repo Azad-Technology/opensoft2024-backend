d438175e43fa0bb875c908df5c78ec9d0a019561
Shivam Kumar <shivamkmishra09@kgpian.iitkgp.ac.in>
2024-03-19 23:25:20 +0530

    password conditions changed

--- src/schemas.py
+++ src/schemas.py
@@ -16,21 +16,6 @@ class UserSignupSchema(BaseModel):
         if len(value) < 8:
             raise ValueError("Password must be at least 8 characters long")
 
-        # Password must contain at least one uppercase letter
-        if not any(char.isupper() for char in value):
-            raise ValueError("Password must contain at least one uppercase letter")
-
-        # Password must contain at least one lowercase letter
-        if not any(char.islower() for char in value):
-            raise ValueError("Password must contain at least one lowercase letter")
-
-        # Password must contain at least one digit
-        if not any(char.isdigit() for char in value):
-            raise ValueError("Password must contain at least one digit")
-
-        # Password must contain at least one special character
-        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", value):
-            raise ValueError("Password must contain at least one special character")
 
         return value
 
