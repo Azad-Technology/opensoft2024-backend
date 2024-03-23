f23c99528c266269f43ada64bb779682b6f16f43
somya-kr <somyakumar958@gmail.com>
2024-03-24 04:12:05 +0530

    Bug Fix: Movie by user IP

--- src/routers/countries.py
+++ src/routers/countries.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Request,Header, status
 from uuid import uuid4
-
+import ipaddress
 from src.db import Movies, countries_dict, projects
 from src import schemas
 from src.config import config
@@ -103,13 +103,11 @@ def get_standardized_country_name(country_name):
 async def get_client_ip(request: Request):
     try:
         forwarded_for = request.headers.get("X-Forwarded-For")
-        print(forwarded_for)
         if forwarded_for:
             client_ip = forwarded_for.split(",")[0]
         else:
             # If X-Forwarded-For header is not present, fallback to request.client.host
             client_ip = request.client.host
-        print(client_ip)
         return client_ip
     except Exception as e:
         raise HTTPException(status_code = 500, detail=str(e))
@@ -120,20 +118,24 @@ async def get_movie_in_my_region(request:Request, count: Optional[int]=10, ip: O
         if not ip:
             ip=await get_client_ip(request)
         if ip:
-            status2, country=get_location_from_ip(ip)
+            ip = ipaddress.ip_address(ip)
+            if ip.is_loopback or ip.is_private:
+                country = 'India'
+                status2=True
+            else: 
+                status2, country=get_location_from_ip(ip)
             if status2:
                 standard_country=get_standardized_country_name(country)
-                print(standard_country)
                 if standard_country:
                     if standard_country in countries_dict:
                         country_to_search=countries_dict[standard_country]
-                        result=await get_movies(country_to_search, count)
+                        result=await get_movies_from_country(country_to_search, count)
                         return result
                     else:
                         return []
                 else:
                     if country in countries_dict['_']:
-                        result=await get_movies(country_to_search, count)
+                        result=await get_movies_from_country(country_to_search, count)
                         return result
                     else:
                         return []
@@ -145,73 +147,7 @@ async def get_movie_in_my_region(request:Request, count: Optional[int]=10, ip: O
         raise http_exc
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
-@router.get('/countries_top_movies/{country_name}/')       #region name case insensitive , count should be optional
-async def get_movies(country_name:str, count: Optional[int] = 10):
-    
-    try:
-        key=country_name+'_'+str(count)+'@'+'country_movies'
-        value = r.get(key)
-        if value:
-            ret=json.loads(value)
-            for re in ret:
-                if 'released' in re:
-                    re['released']=str(re['released'])
-            return ret
-        if count<1:
-           return []
-        default_value = 2
-
-        pipeline = [
-            {
-                "$addFields": {
-                    "imdb.rating": {
-                        "$cond": [
-                            { "$eq": ["$imdb.rating", ""] },
-                            default_value,
-                            "$imdb.rating"
-                        ]
-                    }
-                }
-            },
-            {
-                "$match": {
-                    "countries": {'$regex': f'^{country_name}$', '$options': 'i'},
-                    "type": "movie"
-                }
-            },
-            {
-                "$project": projects
-            },
-            {"$addFields": {
-                "relevance_score": {
-                    "$add": [
-                        {"$multiply": ["$imdb.rating", 10]},
-                        {"$multiply": [{"$abs": {"$subtract":[datetime.now().year , "$year"]}}, -7]},
-                    ]
-                }
-            }},
-            {
-                '$sort': {'relevance_score':-1}
-            },
-            {
-                "$limit": count
-            }
-        ]
-
-        movies_cur = Movies.aggregate(pipeline)  # handle empty strings
-        movies = await movies_cur.to_list(length=None)
-        if movies:
-            for movie in movies:
-                movie['_id']= str(movie['_id'])
-                if 'released' in movie:
-                    movie['released']=movie['released'].strftime('%Y-%m-%d %H:%M:%S')
-            r.set(key,json.dumps(movies))
-            return movies
-        return []
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-                
+              
 
 @router.get('/countries_top_series/{country_name}/')       #region name case insensitive , count should be optional
 async def get_movies(country_name:str, count: Optional[int] = 10):
