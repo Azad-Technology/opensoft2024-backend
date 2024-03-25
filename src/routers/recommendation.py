93d29a0ed50c422ceb3fd8853d77981c81b896e2
somya-kr <somyakumar958@gmail.com>
2024-03-26 03:22:23 +0530

    Cached Movie recommendation

--- src/routers/recommendation.py
+++ src/routers/recommendation.py
@@ -4,6 +4,9 @@ from src import schemas
 from fastapi import APIRouter
 from src.db import Movies
 from src.routers.auth import get_current_user
+from src.cache_system import r
+import json
+
 router = APIRouter()
 
 
@@ -14,11 +17,28 @@ async def recommend_movie(user: dict = Depends(get_current_user)):
     else :
         liked_movies = user['fav']
     try:
+        key = 'recommend_'
+
         if len(liked_movies) == 0:
+            key += 'default'
+            value = r.get(key)
+            if value:
+                return json.loads(value)
             movies = await Movies.find({}, {"_id":1}).sort([("imdb.rating", -1)]).limit(5).to_list(5)
             for movie in movies:
                 liked_movies.append(str(movie["_id"]))
-        recommended_movies = await get_recommendations(liked_movies, 17)
+            recommend_movies = await get_recommendations(liked_movies, 17)
+            r.set(key, json.dumps(recommend_movies))
+            return recommend_movies
+
+        else:
+            key += '_'.join(liked_movies)
+            value = r.get(key)
+            if value:
+                return json.loads(value)
+            recommended_movies = await get_recommendations(liked_movies, 17)
+            r.set(key, json.dumps(recommended_movies))
+            return recommended_movies
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    return recommended_movies
+    
